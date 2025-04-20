e79c411d3a72e00a91c21faac393556af1c7a7f7
rezahy <reza1997hy@yahoo.com>
2025-04-20 17:37:21 +0430

    add-edit/delete-comment-dropdown-menu

--- src/components/comment-list-item.tsx
+++ src/components/comment-list-item.tsx
@@ -1,10 +1,24 @@
 import Image from "next/image";
 import ImageAsset from "@/../public/image-asset.jpeg";
 import { formatDistanceToNow } from "date-fns";
+import { Button } from "./ui/button";
+import {
+	EllipsisVertical,
+	LucideEdit,
+	ThumbsDown,
+	ThumbsUp,
+	Trash,
+} from "lucide-react";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 const CommentListItem = () => {
 	return (
-		<div className="bg-card px-4 py-4 rounded-2xl shadow border">
+		<div className="relative bg-card px-4 py-4 rounded-2xl shadow border">
 			<div className="flex items-center">
 				<Image
 					src={ImageAsset}
@@ -16,32 +30,53 @@ const CommentListItem = () => {
 				<span className="text-sm">username</span>
 			</div>
 			<div className="pt-3">
-				<p className="text-base">
+				<p className="text-base text-justify">
 					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero nobis
 					culpa minima illo accusantium inventore vero nesciunt distinctio nisi
 					cumque. Necessitatibus maxime, provident exercitationem praesentium
 					voluptas hic commodi dicta possimus!
 				</p>
-				<div className="flex justify-between pt-4">
+				<div className="flex justify-between pt-4 items-baseline">
 					<span className="text-xs">
 						{formatDistanceToNow(new Date(), {
 							addSuffix: true,
 						})}
 					</span>
-					<div className="flex space-x-7">
-						{/* <EditCommentButton
-							userData={userData}
-							commentId={comment.id}
-							defaultCommentMessage={comment.message}
-							commentClerkId={commentClerkId}
-						/>
-						<DeleteCommentButton
-							userData={userData}
-							commentId={comment.id}
-							commentClerkId={commentClerkId}
-						/> */}
+					<div className="flex space-x-4">
+						<div className="flex text-xs items-center">
+							<Button variant="ghost">
+								<ThumbsUp />
+							</Button>
+							<span>1000</span>
+						</div>
+						<div className="flex text-xs items-center">
+							<Button variant="ghost">
+								<ThumbsDown />
+							</Button>
+							<span>20</span>
+						</div>
 					</div>
 				</div>
+			</div>
+			<div className="absolute top-3 right-3">
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button variant="ghost">
+							<EllipsisVertical />
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent>
+						<DropdownMenuItem>
+							<LucideEdit />
+							Edit
+						</DropdownMenuItem>
+
+						<DropdownMenuItem>
+							<Trash />
+							Delete
+						</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
 			</div>
 		</div>
 	);
